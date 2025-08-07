f7f411eef23ce5cc2b0e70697a2d01bb16337b6b
Arslaan Juga <arslaanjuga@gmail.com>
2025-08-08 01:30:29 +0500

    vite server fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/SuperReads-/", // ✅ This is the GitHub repo name (case-sensitive, with slashes)
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
